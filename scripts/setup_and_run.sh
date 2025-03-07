343c5943dd357174fd75ed4f89bb76846b132cb5
Pablo Sancho Saiz <pablo.sancho@innovery.es>
2025-03-07 19:00:36 +0100

    Delete non-used packages

--- scripts/setup_and_run.sh
+++ scripts/setup_and_run.sh
@@ -3,7 +3,7 @@
 # Check if venv exists
 if [ ! -d "venv" ]; then
     echo "Creating virtual environment..."
-    python3 -m venv venv
+    python -m venv venv
     source venv/bin/activate
     echo "Installing dependencies..."
     pip install -r requirements.txt
